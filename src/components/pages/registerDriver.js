ed447c1369db78060b30dca178cade4a2d0330e4
chekst <israel.gavriel@gmail.com>
2018-09-08 10:01:37 +0800

    register driver

---
+++ src/components/pages/registerDriver.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class RegisterDriver extends Component {
+    componentWillMount() {
+    }
+    componentDidMount() {
+    }
+    componentWillUpdate() {
+    }
+    render() {
+        return (
+            <div className="home-container">
+            </div>
+        )
+    }
+}
+
+function mapStateToProps (state) {
+    return {
+    }
+}
+
+export default connect(mapStateToProps)(RegisterDriver);
