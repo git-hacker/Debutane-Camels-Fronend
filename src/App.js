6e9a9841c1ab44fb6e505c6e2a4166ca51462987
chekst <israel.gavriel@gmail.com>
2018-09-08 11:12:53 +0800

    home page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import { Route, withRouter } from 'react-router-dom'
 //import store, { history } from './store'
 import { history } from './store'
 //import logo from './logo.svg';
-import './App.css';
-import 'antd/dist/antd.css';
+import './App.css'
+import 'antd/dist/antd.css'
+import { Layout } from 'antd'
 import RegisterDriver from './components/pages/registerDriver'
 import Home from './components/pages/home'
-
+const { Header, Footer, Sider, Content } = Layout;
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" history={history} component={Home} />
-        <Route exact path="/register-driver" history={history} component={RegisterDriver} />
+        <Layout>
+            <Header></Header>
+            <div id='content-wrapper'>
+                <Route exact path="/" history={history} component={Home} />
+                <Route exact path="/register-driver" history={history} component={RegisterDriver} />
+            </div>
+            <Footer></Footer>
+        </Layout>
       </div>
     );
   }
